7f31356c62dc43d2c6e96cf8bac4b6d462f1361e
Nuhu Sulemana <turosung@gmail.com>
2023-06-16 00:27:55 +0100

    Initial Commit

---
+++ Guess_Flag_Proj2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess_Flag_Proj2
+//
+//  Created by Nuhu Sulemana on 16/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
