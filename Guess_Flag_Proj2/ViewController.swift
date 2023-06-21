98f6472d381343a4d2c49f17582b12269700db25
Nuhu Sulemana <turosung@gmail.com>
2023-06-21 01:34:02 +0100

    Started working o layout for Flag Guessing project

--- Guess_Flag_Proj2/ViewController.swift
+++ Guess_Flag_Proj2/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var Button1: UIButton!
+    @IBOutlet var Button2: UIButton!
+    @IBOutlet var Button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
