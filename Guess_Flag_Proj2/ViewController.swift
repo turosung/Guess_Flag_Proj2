7401d1663dc73b2b42dd9f0bae9481c4d1fdbe29
Nuhu Sulemana <turosung@gmail.com>
2023-06-24 21:22:00 +0100

    added flags as buttons to UI

--- Guess_Flag_Proj2/ViewController.swift
+++ Guess_Flag_Proj2/ViewController.swift
@@ -8,15 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var Button1: UIButton!
-    @IBOutlet var Button2: UIButton!
-    @IBOutlet var Button3: UIButton!
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        countries.append("estonia")
+//        countries.append("france")
+//        countries.append("germany")
+//        countries.append("ireland")
+//        countries.append("italy")
+
+// We can also  append all these countries in the countries array in a simplier way
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco","nigeria", "poland", "russia", "spain", "uk","us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.black.cgColor
+        button2.layer.borderColor = UIColor.black.cgColor
+        button3.layer.borderColor = UIColor.black.cgColor
+        
+        askQuestion()
+
     }
+    
+    func askQuestion(){
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
 
-
+    }
+    
 }
 
